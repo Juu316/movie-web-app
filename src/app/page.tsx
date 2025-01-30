e5b8c96b7eaaf81bde270e4fbb28ec9832ba6b1a
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-01-30 12:23:31 +0800

    hahahahahahahahhahahaha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 export default function Home() {
   const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
   const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+  const TMDB_IMAGE_SERVICE_URL=process.env.TMDB_IMAGE_SERVICE_URL ;
   // const options = {
   //   method: 'GET',
   //   headers: {
@@ -13,18 +14,23 @@ export default function Home() {
   //     Authorization: `Bearer ${TMDB_API_TOKEN}`
   //   }
   // };
-  const options = {
-    method: 'GET',
-    url: `${TMDB_BASE_URL}/movie/popular?language=en-US&pafe=1`,
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWFiYzllYjNmZTQxNzI1NDViZDc0MzI2ZmQwMDJmOCIsIm5iZiI6MTczODAzNzc1NC42MzY5OTk4LCJzdWIiOiI2Nzk4NTlmYTM3MmNiMjBjZjgyMzg0NGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WBQLl0E0QJ4_D0cK0QpkcTuzIiyGY7jX3c7QUPBpU-s'
-    }
-  };
-  axios
-  .request(options)
-  .then(res => console.log(res.data))
-  .catch(err => console.error("error here: ",err));
+  
+  function getPopularMovie(){
+    const options = {
+      method: 'GET',
+      url: `${TMDB_BASE_URL}/movie/popular?language=en-US&page=1`,
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${TMDB_API_TOKEN}`
+      }
+    };
+  
+    axios
+    .request(options)
+    .then(res => console.log(res.data))
+    .catch(err => console.error("error here: ",err));
+  }
+  useEffect(getPopularMovie(),[]);
   // const getMovieData = () => {
     
   // //   try {
