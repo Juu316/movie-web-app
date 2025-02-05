55da889ee976108ea443a483fda708eb30ddc9cd
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-02-05 12:31:16 +0800

    jf

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 const Slider = () => {
   //const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
   const TMDB_BASE_URL = "https://api.themoviedb.org/3";
@@ -15,8 +24,8 @@ const Slider = () => {
       )
         .then((res) => res.json())
         .then((json) => {
-          console.log(json);
           setShows(json);
+          console.log("Shows:", json);
         });
       setIsLoading(false);
     } catch (err) {
@@ -27,13 +36,41 @@ const Slider = () => {
   useEffect(() => {
     fetchMovie();
   }, []);
+  const plugin = React.useRef(
+    Autoplay({ delay: 1000, stopOnInteraction: false })
+  );
+  
 
   return (
     <>
-      <div className="relative mt-[59px] lg:mt-[83px] w-screen overflow-hidden">
-        <div className="overflow-hidden">
+      <div className="relative mt-[59px] lg:mt-[83px] w-screen ">
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full max-w-xs"
+          // onMouseEnter={plugin.current.stop}
+          // onMouseLeave={plugin.current.reset}
+          >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        {/* <div className="overflow-hidden">
           <div className="flex -ml-4"></div>
-        </div>
+        </div> */}
       </div>
     </>
   );
